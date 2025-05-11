7baf94f7986614948c424aa63f07e411061ceaf1
Jander Gomez <jandergb.30gmail.com>
2025-05-11 02:09:41 +0200

    Refactor: Add error handling for updateOverdueAccounts in bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { updateOverdueAccounts } from './common/actions/updateOverdueAccounts';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const logger = new Logger('Main');
+
+  await updateOverdueAccounts().catch(err => {
+    logger.error('Error al actualizar estados:', err)
+  })
 
   app.enableCors(
     {
